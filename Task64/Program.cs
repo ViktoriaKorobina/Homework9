48b76a99455956328b528f86b2159cbdf309c4e3
Viktoria <viktoria.korobjina@yandex.ru>
2023-09-30 17:25:09 +0300

    Added task64

---
+++ Task64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string ShowNumbers(int num)
+{
+    if(num == 1) return $"{num} ";
+    else return $"{num} " + ShowNumbers(num - 1); 
+}
+int number = 7;
+System.Console.WriteLine("Ввывод чисел от {0} до 1 в обратном порядке", number);
+Console.WriteLine(ShowNumbers(number));
